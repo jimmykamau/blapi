4b5e7ede735db94ae715a08de3c92040b94c6992
Jimmy Kamau <jimmy.kamau@andela.com>
2016-09-29 10:21:31 +0300

    [Chore #130971199] Create base tests

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
+
 from flask import Flask
+from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -7,6 +9,7 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+api = Api(app)
 
 
 if __name__ == '__main__':
