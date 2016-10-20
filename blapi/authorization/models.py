10a45f32a561b818c4e99c8008a982968576b07b
Jimmy Kamau <jimmy.kamau@andela.com>
2016-10-20 16:29:56 +0300

    [Feature #130971749] Create user login endpoint

--- blapi/authorization/models.py
+++ blapi/authorization/models.py
@@ -1,9 +1,7 @@
 from app import db
 
-from flask_login import UserMixin
 
-
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
